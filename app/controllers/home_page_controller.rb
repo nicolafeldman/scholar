d454b68b7c86a5583cb48a7c3f61d9b2b1cc18fa
nicolafeldman <nicola.feldman@yale.edu>
2014-07-14 10:06:54 -0400

    start to model users

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -10,6 +10,11 @@ class HomePageController < ApplicationController
 	end
 
 	def upload
+		@uploaded_io = params[:picture]
+  		File.open(Rails.root.join('public', 'uploads', @uploaded_io.original_filename), 'wb') do |file|
+    		file.write(@uploaded_io.read)
+  		end
+		# redirect_to 'show'
 	end
 	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Scholar::Application.routes.draw do
+  get "users/new"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   root 'home_page#show'
 
   match '/edithomepage', to: 'home_page#edit', via: 'get'
+  match '/upload', to: 'home_page#upload', via: 'post'
+  match '/signup', to: 'users#new', via: 'get'
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
