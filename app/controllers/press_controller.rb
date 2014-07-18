491cc33091c65b0da1efb7f129d75adc5d9fb97e
nicolafeldman <nicola.feldman@yale.edu>
2014-07-18 15:08:41 -0400

    home-page button goes to press and try to edit text on press page

--- app/controllers/press_controller.rb
+++ app/controllers/press_controller.rb
@@ -1,4 +1,5 @@
 class PressController < ApplicationController
+	before_action :signed_in_user,   only: [:edit, :update] 
 
 	def show
 		@press = Press.find(1)
@@ -7,5 +8,32 @@ class PressController < ApplicationController
     	@quote = quotes.keys.sample
     	@attribution = quotes[@quote]
 	end
+
+	def edit
+	end
+
+	def update
+		@press = Press.find(1)
+  		if @press.update_attributes(press_params)
+  			flash.now[:success] = "Page updated"
+  			render 'show'
+  		else
+  			flash.now[:error] = "Update failed, try again"
+  			render 'show'
+  		end
+	end
+
+	private
+
+		def press_params
+  			params.require(:press).permit(:text)
+  		end
+
+      def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
 
 end
